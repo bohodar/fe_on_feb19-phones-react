9e6e4eaa17f8a4de056a536b33d4f33e40cdb737
Bohodar <bodik264478@gmail.com>
2019-06-08 22:46:32 +0300

    Updates

--- precache-manifest.da9fc02b5f88ce788280c2cca9e401d1.js
+++ precache-manifest.da9fc02b5f88ce788280c2cca9e401d1.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3bccfd664c946a9206a44530c0d6c5f3",
+    "revision": "1904a59bf18ebca815f9b3803eba03e5",
     "url": "/fe_on_feb19-phones-react/index.html"
   },
   {
-    "revision": "a7c066b342f2c74f30bd",
-    "url": "/fe_on_feb19-phones-react/static/css/main.74074b87.chunk.css"
+    "revision": "99931084fe644257ebb0",
+    "url": "/fe_on_feb19-phones-react/static/css/main.af31c9f4.chunk.css"
   },
   {
     "revision": "681b51057d74cfa8975d",
     "url": "/fe_on_feb19-phones-react/static/js/2.65aa1cca.chunk.js"
   },
   {
-    "revision": "a7c066b342f2c74f30bd",
-    "url": "/fe_on_feb19-phones-react/static/js/main.d9da4509.chunk.js"
+    "revision": "99931084fe644257ebb0",
+    "url": "/fe_on_feb19-phones-react/static/js/main.5503e63e.chunk.js"
   },
   {
     "revision": "13ef023f66a5d3f30d63",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fe_on_feb19-phones-react/precache-manifest.4321a3f769abe4bdae7dfdebc8fba4d9.js"
+  "/fe_on_feb19-phones-react/precache-manifest.da9fc02b5f88ce788280c2cca9e401d1.js"
 );
 
 self.addEventListener('message', (event) => {
